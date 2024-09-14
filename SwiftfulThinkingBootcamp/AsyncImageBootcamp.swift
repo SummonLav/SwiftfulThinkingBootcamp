900550c029a49594c4ace222462566f0477759fd
LavendaShan <79836722+SummonLav@users.noreply.github.com>
2024-09-14 15:11:27 -0400

    Update AsyncImageBootcamp.swift

--- SwiftfulThinkingBootcamp/AsyncImageBootcamp.swift
+++ SwiftfulThinkingBootcamp/AsyncImageBootcamp.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct AsyncImageBootcamp: View {
+    let url = URL(string: "https://picsum.photos/400")
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        AsyncImage(url: url) { ImagePhase in
+            switch ImagePhase {
+            case .empty:
+                ProgressView()
+            case .success(let returnedImage):
+                returnedImage
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+                    .cornerRadius(20.0)
+            case .failure:
+                Image(systemName: "questionmark")
+                    .font(.headline)
+            default:
+                Image(systemName: "questionmark")
+                    .font(.headline)
+            }
+        }
+        //        AsyncImage(url: url, content: { returnedImage in
+        //            returnedImage
+        //                .resizable()
+        //                .scaledToFit()
+        //                .frame(width: 200, height: 200)
+        //                .cornerRadius(20.0)
+        //        }, placeholder: {
+        //            ProgressView()
+        //        })
     }
 }
 
